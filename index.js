1bec3803ed972afc99ed05d1d23b0b376507b3e7
vaibhavdkjbdjchv <118902119+vaibhavdkjbdjchv@users.noreply.github.com>
2024-10-13 16:18:25 +0530

    update

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-console.log("hellow vaibhav");
-console.log("hellow vaibhav");
+// const user = new Object();
+// user.name = "vaibhav"
+// user.age = 30
+// user["city"] = "New York"
+// console.log(user);
+
+// // diffrent way of creationg object
+// const user2 = {
+//     name: 'John',
+//     age: 30,
+//     occupation: 'Developer',
+// }
+// console.log(user2);
+// console.log(user2.name);
+
+
+
+
+const obj1 = { 1: "a", 2: "9" }
+const obj2 = { 3: "a", 4: "9" }
+// const obj3 = { obj1, obj2 }
+const obj3 =Object.assign({} ,obj1,obj2)
+const obj4= {...obj1,...obj2} //sprade object
+console.log(obj3);
