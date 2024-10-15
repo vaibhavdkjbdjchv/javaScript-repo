ef2a75baa020c746cf0d847c05408e2a6f596006
vitthal <118902119+vaibhavdkjbdjchv@users.noreply.github.com>
2024-10-15 14:27:50 +0000

    update

--- if else.js
+++ if else.js
@@ -10,9 +10,24 @@ const user1 = {
 }
 
 
+
+
+
+
 if (Object.keys(user1).length === 0) {
     console.log("this object is empty");
 }
 else {
     console.log("not an empty object");
 }
+
+
+// Nullish coalescing oprator (??): null undefined 
+
+let val1;
+val1 = null ?? 2;
+val2 = 5 ?? 6;
+
+console.log(`two values ${val2}`);
+console.log(`value with null ${val1}`);
+
