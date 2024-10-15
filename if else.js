4d7bb1fa2047c1c659062ad4a4cc25424e3d2237
vitthal <118902119+vaibhavdkjbdjchv@users.noreply.github.com>
2024-10-15 14:19:15 +0000

    update

--- if else.js
+++ if else.js
@@ -1,2 +1,18 @@
-const a=2
-if (a==1?console.log("yes"):console.log("no"));
+const a = 2
+if (a == 1 ? console.log("yes") : console.log("no"));
+
+
+// object 
+
+const user1 = {
+    a: 0
+
+}
+
+
+if (Object.keys(user1).length === 0) {
+    console.log("this object is empty");
+}
+else {
+    console.log("not an empty object");
+}
