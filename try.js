eaabb4b073ace685b809f85c79b13c9df7ec96f2
vitthal <118902119+vaibhavdkjbdjchv@users.noreply.github.com>
2024-10-17 07:21:49 +0000

    update

--- try.js
+++ try.js
@@ -1,1 +1,10 @@
 console.log(":hi");
+const mySum =[5,2,3];
+
+
+const myTotal = mySum.reduce((acc, currval)=>{
+    console.log(`acc := ${acc} Curval:=  ${currval}`);
+    return acc+currval
+},0);
+
+console.log(myTotal);
