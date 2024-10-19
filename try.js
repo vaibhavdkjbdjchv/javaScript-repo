38c382bc6aab9d8cd4bcbb24b5b3ef4fad9c2021
vitthal <118902119+vaibhavdkjbdjchv@users.noreply.github.com>
2024-10-19 05:31:21 +0000

    update

--- try.js
+++ try.js
@@ -1,10 +1,40 @@
-console.log(":hi");
-const mySum =[5,2,3];
+// console.log(":hi");
+// const mySum = [5, 2, 3];
 
 
-const myTotal = mySum.reduce((acc, currval)=>{
-    console.log(`acc := ${acc} Curval:=  ${currval}`);
-    return acc+currval
-},0);
+// const myTotal = mySum.reduce((acc, currval) => {
+//     console.log(`acc := ${acc} Curval:=  ${currval}`);
+//     return acc + currval
+// }, 0);
 
-console.log(myTotal);
+// console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js cours",
+        price: 5000
+
+    },
+    {
+        itemName: "puthon cours",
+        price: 4000
+
+    },
+    {
+        itemName: "java cours",
+        price: 10000
+
+    },
+    {
+        itemName: "fullstack cours",
+        price: 20000
+
+    },
+
+]
+
+const addAllPrices = shoppingCart.reduce((acc, item) => {
+    console.log(`acc = ${acc} currantValue = ${item.price}`);
+    return acc + item.price;
+}, 0);
+console.log(addAllPrices);
